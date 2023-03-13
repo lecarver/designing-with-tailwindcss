b7339b52429fd3a5f689164b1ec355ce59b1620e
lecarver <105886770+lecarver@users.noreply.github.com>
2023-03-13 19:56:49 +0000

    complete pt2 of nav bar

--- 03-building-a-responsive-navbar/02-toggling-the-navbar-links-on-mobile/src/components/Navbar.vue
+++ 03-building-a-responsive-navbar/02-toggling-the-navbar-links-on-mobile/src/components/Navbar.vue
@@ -1,31 +1,35 @@
 <template>
   <header class="bg-gray-900">
-    <div class="flex items-center justify-between px-4 py-3">
+    <div class="flex items-center justify-between px-4 py-3 bg-gray-900">
       <div>
-        <img class="h-8" src="/img/logo-inverted.svg" alt="Workcation">
+        <img class="h-8" src="img/logo-inverted.svg" alt="Workcation">
       </div>
+
       <div>
-        <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+        <!-- add @click so that our 'isopen' will switch to true and open the navbar -->
+        <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 focus:text-white focus:outline-none hover:text-white">
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <!-- Change menu bar icon based on whether it is open or not -->
             <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
           </svg>
         </button>
       </div>
     </div>
+    <!-- if isOpen is true then the navbar shows but is hidden by default -->
     <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4">
-      <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
-      <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Trips</a>
-      <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Messages</a>
+      <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Trips</a>
+      <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
+      <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Message</a>
     </div>
-  </header>
+  </header>   
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isOpen: false,
+      isOpen:false,
     }
   },
 }
